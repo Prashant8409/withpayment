7a2aa63b799e7c176a682627a3c61f77369f9047
Prashant8409 <68659880+Prashant8409@users.noreply.github.com>
2025-12-12 21:09:20 +0530

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -23,15 +23,21 @@ export const products: Product[] = [
     sellerYears: "5+ Years on Jiji"
   },
   {
-    id: "2",
-    title: "Toyota Vanguard 2008 Black",
-    price: 28500000,
-    location: "Dar es Salaam, Kinondoni",
-    image: "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?w=400&h=300&fit=crop",
-    description: "sdhcjbsdh",
-    condition: "Local Used",
-    transmission: "Automatic"
-  },
+  id: "2",
+  title: "Toyota Vanguard 2008 Black",
+  price: 28500000,
+  location: "Dar es Salaam, Kinondoni",
+  image: "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?w=400&h=300&fit=crop",
+  description: `
+_iPhone 16 Pro Max_
+_Storage: 256gb_
+_Model: J/A_
+_BH: 91%_
+_Apple Warranty_.
+  `,
+  condition: "Local Used",
+  transmission: "Automatic"
+},
   {
     id: "3",
     title: "BMW 1 Series 2013 Blue",
